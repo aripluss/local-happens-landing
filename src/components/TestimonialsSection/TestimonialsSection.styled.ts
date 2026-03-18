d5881aeacf0ce4638c61f430df416b322385e8de
Iryna Klosheva <aripluss@i.ua>
2026-03-18 19:01:51 +0200

    fix conflicts

--- src/components/TestimonialsSection/TestimonialsSection.styled.ts
+++ src/components/TestimonialsSection/TestimonialsSection.styled.ts
@@ -32,11 +32,7 @@ export const StyledTestimonialsContainer = styled(StyledContainer)`
     flex-direction: column;
     justify-content: space-between;
     gap: 16px;
-<<<<<<< HEAD
-    background-color: var(--bg);
-=======
     background-color: white;
->>>>>>> 459ca6d (DEV-19 add preview sections, fix styles)
     padding: 24px 16px;
     border-radius: ${theme.radii.normal};
     box-shadow: var(--shadow);
@@ -52,10 +48,6 @@ export const StyledTestimonialsContainer = styled(StyledContainer)`
   .text {
     color: var(--text-light);
     font-style: italic;
-<<<<<<< HEAD
-    /* font-size: ${theme.fontSizes.m}; */
-=======
->>>>>>> 459ca6d (DEV-19 add preview sections, fix styles)
   }
 
   .author {
--- src/components/TestimonialsSection/TestimonialsSection.tsx
+++ src/components/TestimonialsSection/TestimonialsSection.tsx
@@ -1,14 +1,7 @@
-import React from "react";
-<<<<<<< HEAD
-import { StyledTestimonialsContainer } from "./TestimonialsSection.styled";
-import { GiRoundStar } from "react-icons/gi";
-
-=======
 import { GiRoundStar } from "react-icons/gi";
 
 import { StyledTestimonialsContainer } from "./TestimonialsSection.styled";
 
->>>>>>> 459ca6d (DEV-19 add preview sections, fix styles)
 interface TestimonialItem {
   rating: number;
   text: string;
@@ -47,14 +40,10 @@ export const TestimonialsSection: React.FC = () => {
   return (
     <section id="testimonials" className="testimonials">
       <StyledTestimonialsContainer>
-<<<<<<< HEAD
-        <h2 className="title">Відгуки наших користувачів</h2>
-=======
         <h2 className="title">
           Відгуки від реальних користувачів,
           <br /> які вже інтегрували LocalHappens у своє життя
         </h2>
->>>>>>> 459ca6d (DEV-19 add preview sections, fix styles)
 
         <div className="cards">
           {testimonialData.map((item, index) => (
@@ -67,11 +56,7 @@ export const TestimonialsSection: React.FC = () => {
                     </span>
                   ))}
                 </div>
-<<<<<<< HEAD
-                <p className="text">"{item.text}"</p>
-=======
                 <p className="text">“{item.text}”</p>
->>>>>>> 459ca6d (DEV-19 add preview sections, fix styles)
               </div>
               <div className="author">
                 <p className="author-name">{item.author}</p>
